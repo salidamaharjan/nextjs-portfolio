8317f38ae54b6266b39c1db03ef580f91aa16123
salidamaharjan <salidamaharjan@outlook.com>
2024-06-11 14:18:19 -0500

    Add image and introduction to portfolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
 import {Github, Linkedin} from 'lucide-react';
 import DetailSection from "@/components/ui/DetailSection";
+import Image from "next/image";
 export default function Home() {
     return (
         <div className="flex flex-col justify-evenly gap-2 p-6">
+            <div>
+            <div className="font-normal text-xs">
+                Hello
+            </div>
+            <div className="font-medium"> I am Salida</div>
+            <div className="text-sm font-normal">Junior Full Stack Software Developer</div>
+            <div>
+                <Image className="rounded-md" width={200} height={200} src="/img.jpg" alt="My image"/>
+            </div>
+            </div>
             <DetailSection id="about-me">
                 About Me
             </DetailSection>
