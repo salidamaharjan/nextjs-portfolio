73babd219ab42b4010d1352ef43686f1c00e6651
salidamaharjan <salidamaharjan@outlook.com>
2024-06-11 23:30:03 -0500

    Update CSS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,11 +8,11 @@ export default function Home() {
             <div
                 className="flex justify-evenly items-center bg-fixed bg-[url('/background.png')] md:h-[500px] h-[100px]">
                 <div>
-                    <div className="font-normal md:text-md text-xs">
+                    <div className="font-normal md:text-lg text-xs">
                         Hello
                     </div>
                     <div className="font-medium md:text-2xl text:md"> I am Salida</div>
-                    <div className="md:text-lg text-xs font-normal">Junior Full Stack Software Developer</div>
+                    <div className="font-normal md:text-md text-xs">Junior Full Stack Software Developer</div>
                 </div>
                 <div>
                     <Image className="rounded-md md:w-[300px] md:h-[300px]" width={90} height={90} src="/img.jpg"
@@ -28,7 +28,7 @@ export default function Home() {
                         apply my programming skills and contribute to innovative projects.
                     </div>
                     <div className="font-normal md:text-md text-sm">
-                        <div className="md:font-medium font-medium md:text-lg">My Skills</div>
+                        <div className="md:font-medium font-medium text-red-700 text-sm md:text-md">My Skills</div>
                         <ul className="pl-4">
                             <li className="list-disc"> Proficient in HTML, CSS, JavaScript.
                             </li>
@@ -53,39 +53,45 @@ export default function Home() {
                 <DetailSection id="projects">
                     <div>Projects</div>
                     <div className="font-normal md:text-sm text-xs">
-                        <div className="md:font-medium font-medium text-sm md:text-md">Food and Recipes</div>
+                        <div className="md:font-medium font-medium text-sm md:text-md text-red-700">Food and Recipes
+                        </div>
                         This app allows users to search an ingredient, receive the nutrition information and recipes.
                         Users can save recipes to their favorites.
-                        <div className="md:font-medium font-medium text-sm md:text-md">Expenses Tracker </div>
+                        <div className="md:font-medium font-medium text-sm md:text-md text-red-700">Expenses
+                            Tracker </div>
                         This app allows the user to add the expenses and track it. The expenses trend can be seen in the
                         line graph and pie chart.
-                        <div className="md:font-medium font-medium text-sm md:text-md">Weather Dashboard App </div>
+                        <div className="md:font-medium font-medium text-sm md:text-md text-red-700">Weather Dashboard
+                            App </div>
                         This app shows the weather for the current day and up to next 5 days. Data is retrieved from
                         open weather API.
-                        <div className="md:font-medium font-medium text-sm md:text-md">Employee Tracker SQL</div>
+                        <div className="md:font-medium font-medium text-sm md:text-md text-red-700">Employee Tracker
+                            SQL</div>
                         The user can view all the employee from DB, add employee, update role, manager and view the
                         budget stored in DB. User can delete the data as well.
-                        <div className="md:font-medium font-medium text-sm md:text-md">Note Taker Express.js</div>
+                        <div className="md:font-medium font-medium text-sm md:text-md text-red-700">Note Taker
+                            Express.js</div>
                         The user can use this app to create a note, edit, delete and save according to work plan.
-                        <div className="md:font-medium font-medium text-sm md:text-md">PWA Text Editor</div>
+                        <div className="md:font-medium font-medium text-sm md:text-md text-red-700">PWA Text
+                            Editor</div>
                         This app can be used offline and is downloadable. User can add text, edit and save them.
                     </div>
                 </DetailSection>
                 <DetailSection id="education">
                     <div>Education</div>
                     <div className="font-normal md:text-sm text-xs">
-                        <div className="md:font-medium font-medium text-sm md:text-md">Full Stack Web Development Boot
+                        <div className="md:font-medium font-medium text-sm md:text-md text-red-700">Full Stack Web Development Boot
                             Camp
                         </div>
                         University Of Minnesota partnership with edX Minneapolis,
                         MN
-                        <div className="md:font-medium font-medium text-sm md:text-md">Bachelor of Science in Software
+                        <div className="md:font-medium font-medium text-sm md:text-md text-red-700">Bachelor of Science in Software
                             Development</div> Grand Canyon University
                         Phoenix, AZ
-                        <div className="md:font-medium font-medium text-sm md:text-md">Post Basic Bachelor in
+                        <div className="md:font-medium font-medium text-sm md:text-md text-red-700">Post Basic Bachelor in
                             Nursing</div>
                         Kathmandu Model College of Nursing Kathmandu, Nepal
-                        <div className="md:font-medium font-medium text-sm md:text-md">Associate Degree in Nursing</div>
+                        <div className="md:font-medium font-medium text-sm md:text-md text-red-700">Associate Degree in Nursing</div>
                         Manmohan Memorial Institute of Health Sciences Kathmandu, Nepal
                     </div>
                 </DetailSection>
